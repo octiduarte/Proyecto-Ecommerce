c2ef12dc7775362a32abb29421fa2775cdd7cf88
octiduarte <octiduarte@hotmail.com>
2024-08-23 11:41:56 -0300

    ,

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <button className="text-blue-600 text-3xl sm:text-5xl">Click me</button>
+      <h1 className="text-2xl md:text-4xl lg:text-6xl"></h1>
+    </main>
+  );
+}
