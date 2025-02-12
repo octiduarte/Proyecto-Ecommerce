370948b1ecbc3a4759156ac1365a15c9b65c8b61
Octavio Duarte <octiduarte@hotmail.com>
2025-02-12 12:18:29 -0300

    vuelta atras

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,8 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-
 export async function fetchData() {
-  const res = await fetch(`${API_URL}/main/stores/1`, {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  return data;
-}
+    const res = await fetch('http://localhost:8000/main/stores/1', {
+      cache: 'no-store' // Esto deshabilita la caché, similar a getServerSideProps.
+    });
+    const data = await res.json();
+    return data;
+  }
+
