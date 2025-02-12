550435e107d9726ed79f4641d4d7b90a9ad57407
Octavio Duarte <octiduarte@hotmail.com>
2025-02-12 09:12:41 -0300

    cambio url

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,7 @@
 export async function fetchData() {
-    const res = await fetch('http://localhost:8000/main/stores/1', {
-      cache: 'no-store' // Esto deshabilita la caché, similar a getServerSideProps.
-    });
-    const data = await res.json();
-    return data;
-  }
-
+  const res = await fetch('https://ecommercebd-production.up.railway.app/main/stores/1', {
+    cache: 'no-store'
+  });
+  const data = await res.json();
+  return data;
+}
