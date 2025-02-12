c3e19177d943fdab19ec6392a8f392cf586abff1
Octavio Duarte <octiduarte@hotmail.com>
2025-02-12 11:39:29 -0300

    .

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,8 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
 export async function fetchData() {
-  const res = await fetch('https://ecommercebd-production.up.railway.app/main/stores/1', {
-    cache: 'no-store'
+  const res = await fetch(`${API_URL}/main/stores/1`, {
+    cache: "no-store",
   });
   const data = await res.json();
   return data;
